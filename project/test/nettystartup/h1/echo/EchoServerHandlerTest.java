75a06897777485c3cdfcdd653f00b2f7d24c9cfa
Daehyun Kim <hatemogi@gmail.com>
2015-03-23 10:39:33 +0900

    첫번째시간 다듬기

--- project/test/nettystartup/h1/echo/EchoServerHandlerTest.java
+++ project/test/nettystartup/h1/echo/EchoServerHandlerTest.java
@@ -3,6 +3,7 @@ package nettystartup.h1.echo;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.util.CharsetUtil;
 import nettystartup.h1.echo.EchoServerHandler;
 import org.junit.Test;
 
@@ -12,14 +13,14 @@ import static org.junit.Assert.assertThat;
 
 public class EchoServerHandlerTest {
     @Test
-    public void testEcho() {
+    public void echo() {
         String m = "echo test\n";
         EmbeddedChannel ch = new EmbeddedChannel(new EchoServerHandler());
-        ByteBuf in = Unpooled.wrappedBuffer(m.getBytes());
+        ByteBuf in = Unpooled.copiedBuffer(m, CharsetUtil.UTF_8);
         ch.writeInbound(in);
         ByteBuf r = (ByteBuf)ch.readOutbound();
         releaseLater(r);
         assertThat(r.refCnt(), is(1));
-        assertThat(new String(r.array()), equalTo(m));
+        assertThat(r.toString(CharsetUtil.UTF_8), equalTo(m));
     }
 }
