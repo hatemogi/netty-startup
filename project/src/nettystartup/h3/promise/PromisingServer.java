0c26cae1ab000ae5a20db1827dbac735d219c9ce
Daehyun Kim <hatemogi@gmail.com>
2015-03-25 01:04:30 +0900

    promise 예제

--- project/src/nettystartup/h3/promise/PromisingServer.java
+++ project/src/nettystartup/h3/promise/PromisingServer.java
@@ -1,4 +1,22 @@
 package nettystartup.h3.promise;
 
-public class PromisingServer {
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import nettystartup.h2.NettyStartupUtil;
+
+public final class PromisingServer {
+    public static void main(String[] args) throws Exception {
+        NettyStartupUtil.runServer(8031,  new ChannelInitializer<SocketChannel>() {
+            @Override
+            public void initChannel(SocketChannel ch) {
+                ch.pipeline()
+                        .addLast(new LineBasedFrameDecoder(1024))
+                        .addLast(new StringDecoder(), new StringEncoder())
+                        .addLast(new PromisingServerHandler());
+            }
+        });
+    }
 }
--- project/src/nettystartup/h3/promise/PromisingServerHandler.java
+++ project/src/nettystartup/h3/promise/PromisingServerHandler.java
@@ -1,4 +1,37 @@
 package nettystartup.h3.promise;
 
-public class PromisingServerHandler {
+import io.netty.channel.*;
+
+class PromisingServerHandler extends SimpleChannelInboundHandler<String> {
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, String line) {
+        System.out.println(line);
+        System.out.println("channelRead0: " + Thread.currentThread().getName());
+        ctx.write("> " + line + "\n");
+        ChannelPromise p = ctx.newPromise();
+        Runnable block = () -> {
+            try {
+                Thread.sleep(5000);
+                System.err.println("new Thread: " + Thread.currentThread().getName());
+                p.setSuccess();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        new Thread(block).start();
+        p.addListener(f ->
+            System.out.println("listener:" + Thread.currentThread().getName())
+        );
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
