40a2f815aa99e90fd8cf65a4cf6ac1db6038e5b4
Daehyun Kim <hatemogi@gmail.com>
2016-10-25 21:50:01 +0900

    서버 구동 코드 정리

--- src/nettystartup/h2/http/HttpStaticServer.java
+++ src/nettystartup/h2/http/HttpStaticServer.java
@@ -1,8 +1,5 @@
 package nettystartup.h2.http;
 
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import nettystartup.h2.NettyStartupUtil;
@@ -11,16 +8,12 @@ class HttpStaticServer {
     final static String index = System.getProperty("user.dir") + "/res/h2/index.html";
 
     public static void main(String[] args) throws Exception {
-        NettyStartupUtil.runServer(8020, new ChannelInitializer<SocketChannel>() {
-            @Override
-            public void initChannel(SocketChannel ch) {
-                ChannelPipeline p = ch.pipeline();
-                p.addLast(new HttpServerCodec());
-                p.addLast(new HttpObjectAggregator(65536));
-                p.addLast(new HttpStaticFileHandler("/", index));
-                // TODO: [실습2-2] HttpNotFoundHandler를 써서 404응답을 처리합니다.
+        NettyStartupUtil.runServer(8020, pipeline -> {
+            pipeline.addLast(new HttpServerCodec());
+            pipeline.addLast(new HttpObjectAggregator(65536));
+            pipeline.addLast(new HttpStaticFileHandler("/", index));
+            // TODO: [실습2-2] HttpNotFoundHandler를 써서 404응답을 처리합니다.
 
-            }
         });
     }
 }
