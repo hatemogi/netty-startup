fc5e19d40d917fb1c168a4ddbd2f41a0b47b2379
Daehyun Kim <hatemogi@gmail.com>
2016-10-25 21:46:28 +0900

    HTTP 서버 테스트케이스 강화

--- test/nettystartup/h2/http/HttpStaticFileHandlerTest.java
+++ test/nettystartup/h2/http/HttpStaticFileHandlerTest.java
@@ -5,18 +5,29 @@ import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.codec.http.*;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class HttpStaticFileHandlerTest {
     @Test
-    public void index() throws Exception {
+    public void indexAndNotFound() throws Exception {
         String index = HttpStaticServer.index;
-        EmbeddedChannel ch = new EmbeddedChannel(new HttpStaticFileHandler("/", index));
+        EmbeddedChannel ch = new EmbeddedChannel(
+                new HttpStaticFileHandler("/", index),
+                new HttpNotFoundHandler());
         ch.writeInbound(new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/"));
-        assertThat(ch.readOutbound(), instanceOf(HttpResponse.class));
+        HttpResponse res = ch.readOutbound();
+        assertThat(res.status(), equalTo(HttpResponseStatus.OK));
+
         FileRegion content = ch.readOutbound();
         assertTrue(content.count() > 0);
+        assertThat(ch.readOutbound(), instanceOf(LastHttpContent.class));
+
+        ch.writeInbound(new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/should-be-not-found"));
+        HttpResponse res404 = ch.readOutbound();
+        assertThat("404 응답 확인", res404, instanceOf(HttpResponse.class));
+        assertThat("404 NOT FOUND 상태 코드 확인", res404.status(), equalTo(HttpResponseStatus.NOT_FOUND));
     }
 }
